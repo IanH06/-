24bf005d8f71d7f2167fe4ffce9b3464c0f25870
IanH06 <Ian.Hon@Abingdon.Org.Uk>
2023-09-14 14:34:04 +0100

    .

--- NEA_UI.py
+++ NEA_UI.py
@@ -8,9 +8,10 @@ class MainWindow(qtw.QMainWindow):
     def __init__(self):
         self.page = 0
         super().__init__()
-        self.setWindowTitle("Wassup")
-        self.fl = qtw.QGridLayout()
-        self.fl2 = qtw.QGridLayout()
+        self.setWindowTitle("Notes Taking App")
+
+        self.menuGrid = qtw.QGridLayout()
+        self.deckSelect = qtw.QGridLayout()
 
         self.user_list  = qtw.QComboBox()
         self.users = qtw.QListWidget()
@@ -24,8 +25,11 @@ class MainWindow(qtw.QMainWindow):
         self.removeUser = qtw.QPushButton("Remove Selected User")
         self.removeUser.clicked.connect(self.remove)
 
-        self.swappage = qtw.QPushButton("Swap Pages?")
-        self.swappage.clicked.connect(self.swapPage)
+        self.startB = qtw.QPushButton("Start")
+        self.startB.clicked.connect(self.deckList)
+
+        self.selectDeck = qtw.QPushButton("Use Selected Deck")
+        self.selectDeck.clicked.connect(self.openDeck)
 
         sql = """ SELECT * FROM users"""
         c = userdb_connect.execute(sql)
@@ -33,25 +37,24 @@ class MainWindow(qtw.QMainWindow):
             self.users.addItem("Ayou")
             self.user_list.addItem(f"{i[1]} [{i[0]}]")
 
-        self.fl2.addWidget(self.users,0,0,1,1)
-        self.fl2.addWidget(self.swappage,1,0,1,1)
-        self.fl.addWidget(self.swappage,2,0,1,3)
+        self.deckSelect.addWidget(self.users,0,0,1,1)
+        
+        self.menuGrid.addWidget(self.user_list,0,0,1,3)
+        self.menuGrid.addWidget(self.add_user,1,0,1,1)
+        self.menuGrid.addWidget(self.new_user_button,1,1,1,1)
+        self.menuGrid.addWidget(self.removeUser,1,2,1,1)
+        self.menuGrid.addWidget(self.startB,2,0,1,3)
 
-        self.fl.addWidget(self.user_list,0,0,1,3)
-        self.fl.addWidget(self.add_user,1,0,1,1)
-        self.fl.addWidget(self.new_user_button,1,1,1,1)
-        self.fl.addWidget(self.removeUser,1,2,1,1)
+        self.WDeck = qtw.QWidget()
+        self.WDeck.setLayout(self.deckSelect)
 
-        self.widget = qtw.QWidget()
-        self.widget.setLayout(self.fl)
-        self.setCentralWidget(self.widget)
+        self.start = qtw.QWidget()
+        self.start.setLayout(self.menuGrid)
+        self.setCentralWidget(self.start)
 
-    def swapPage(self):
-        if self.page == 0:
-            self.setLayout(self.fl2)
-            self.page = 1
-        else:
-            self.setLayout(self.fl)
+    def deckList(self):
+        self.setCentralWidget(self.WDeck)
+
 
     def getNextUID(self):
         sql = """ SELECT * FROM users"""
@@ -87,11 +90,19 @@ class MainWindow(qtw.QMainWindow):
     def addUser(self):
         nextID = self.getNextUID()
         username =  self.add_user.text()
-        self.user_list.addItem(f"{username} [{nextID}]")
-        f = [nextID, username]
-        sql = ''' INSERT INTO users (UID, Username) VALUES(?,?)'''
-        c = userdb_connect.execute(sql, tuple(f))
-        userdb_connect.commit()
+        if username:
+            self.user_list.addItem(f"{username} [{nextID}]")
+            f = [nextID, username]
+            sql = ''' INSERT INTO users (UID, Username) VALUES(?,?)'''
+            c = userdb_connect.execute(sql, tuple(f))
+            userdb_connect.commit()
+        else:
+            no_name = qtw.QMessageBox()
+            no_name.setIcon(qtw.QMessageBox.Warning)
+            no_name.setText(f"Please enter a valid username.")
+            no_name.setWindowTitle("No username entered")
+            no_name.setStandardButtons(qtw.QMessageBox.Ok)
+            no_name.exec()
 
 
         
